18dcf1a07f327cb4d5d0d9b117d9bd934b0e21c4
MateuszGawel <mg.mateuszgawel@gmail.com>
2013-01-24 20:03:34 +0100

    GUI

--- src/people/Aplet.java
+++ src/people/Aplet.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 import java.util.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import netscape.javascript.JSObject;
 import java.lang.*;
@@ -23,15 +25,22 @@ public class Aplet extends JApplet implements ActionListener
     //formularz
     private JTextField login = null;
     private JTextField password = null;
-    private JTextField forname = null;
-    private JTextField surname = null;
+    private JTextField forename = null;
+    private JTextField surename = null;
     private JTextField empDate = null;
     private JTextField earnings = null;
-    private JTextField worktime = null;
+    private JTextField workTime = null;
     private JTextField nip = null;
     private JTextField account = null;
-    private JTextField name = null; //do korespondencji
     private JButton zarejestruj;
+    private JTextField DataName = null;
+    private JTextField dataDetails = null;
+    private JTextField zipCode = null;
+    private JTextField city = null;
+    private JTextField tel = null;
+    private JTextField mail = null;
+    private JTextField baseid = null;
+    private JTextField dataCountryId = null;
 
 
     Aplet aplet = this;
@@ -88,7 +97,7 @@ public class Aplet extends JApplet implements ActionListener
         }
         else if(e.getSource() == zapytanko)
         {
-            CachedRowSetImpl zapytanie = klient.communicationMySQL("select * from user");
+            CachedRowSetImpl zapytanie = klient.request("select * from user");
             try {
                 napis.setText(zapytanie.getString(2));
             } catch (SQLException e1) {
@@ -100,34 +109,68 @@ public class Aplet extends JApplet implements ActionListener
             napis.setText(" ");
             remove(rejestracjaPracownika);
             setLayout(new FlowLayout());
+
             login = new JTextField("Login");
             password = new JTextField("Haslo");
-            forname = new JTextField("Imię");
-            surname = new JTextField("Nazwisko");
+            forename = new JTextField("Imię");
+            surename = new JTextField("Nazwisko");
             empDate = new JTextField("Data Zatrudnienia");
             earnings = new JTextField("Zarobki");
-            worktime = new JTextField("Czas Pracy");
+            workTime = new JTextField("Czas Pracy");
             nip = new JTextField("NIP");
             account = new JTextField("nr Konta");
-            name = new JTextField("Adres Korespondencyjny");
+            DataName = new JTextField("DataName");
+            dataDetails = new JTextField("dataDetails");
+            zipCode = new JTextField("Kod Pocztowy");
+            city = new JTextField("Miasto");
+            tel = new JTextField("Telefon");
+            mail = new JTextField("E-mail");
+            baseid = new JTextField("baseid");
+            dataCountryId =  new JTextField("dataCountryId");
 
-            add(zarejestruj);
+            login.addMouseListener(new MouseAdapter(){@Override public void mouseClicked(MouseEvent e){login.setText("");}  });
+            password.addMouseListener(new MouseAdapter(){@Override public void mouseClicked(MouseEvent e){password.setText("");}  });
+            forename.addMouseListener(new MouseAdapter(){@Override public void mouseClicked(MouseEvent e){forename.setText("");}  });
+            surename.addMouseListener(new MouseAdapter(){@Override public void mouseClicked(MouseEvent e){surename.setText("");}  });
+            empDate.addMouseListener(new MouseAdapter(){@Override public void mouseClicked(MouseEvent e){empDate.setText("");}  });
+            earnings.addMouseListener(new MouseAdapter(){@Override public void mouseClicked(MouseEvent e){earnings.setText("");}  });
+            baseid.addMouseListener(new MouseAdapter(){@Override public void mouseClicked(MouseEvent e){baseid.setText("");}  });
+            workTime.addMouseListener(new MouseAdapter(){@Override public void mouseClicked(MouseEvent e){workTime.setText("");}  });
+            nip.addMouseListener(new MouseAdapter(){@Override public void mouseClicked(MouseEvent e){nip.setText("");}  });
+            account.addMouseListener(new MouseAdapter(){@Override public void mouseClicked(MouseEvent e){account.setText("");}  });
+            DataName.addMouseListener(new MouseAdapter(){@Override public void mouseClicked(MouseEvent e){DataName.setText("");}  });
+            dataCountryId.addMouseListener(new MouseAdapter(){@Override public void mouseClicked(MouseEvent e){dataCountryId.setText("");}  });
+            dataDetails.addMouseListener(new MouseAdapter(){@Override public void mouseClicked(MouseEvent e){dataDetails.setText("");}  });
+            zipCode.addMouseListener(new MouseAdapter(){@Override public void mouseClicked(MouseEvent e){zipCode.setText("");}  });
+            city.addMouseListener(new MouseAdapter(){@Override public void mouseClicked(MouseEvent e){city.setText("");}  });
+            tel.addMouseListener(new MouseAdapter(){@Override public void mouseClicked(MouseEvent e){tel.setText("");}  });
+            mail.addMouseListener(new MouseAdapter(){@Override public void mouseClicked(MouseEvent e){mail.setText("");}  });
+
             add(login);
             add(password);
-            add(forname);
-            add(surname);
+            add(forename);
+            add(surename);
             add(empDate);
             add(earnings);
-            add(worktime);
+            add(baseid);
+            add(workTime);
             add(nip);
             add(account);
-            add(name);
-
+            add(DataName);
+            add(dataCountryId);
+            add(dataDetails);
+            add(zipCode);
+            add(city);
+            add(tel);
+            add(mail);
+            add(zarejestruj);
         }
         else if(e.getSource() == zarejestruj)
         {
-           // new Pracownik(login, password, forname, surname, empDate, earnings, worktime, nip, account, name); //wg konstruktora
+            int baseidInt = Integer.parseInt(mail.getText());
+            int dataCountryIdInt = Integer.parseInt(dataCountryId.getText());
+            int zipCodeInt = Integer.parseInt(zipCode.getText());
+            new Employee(login.getText(), password.getText(), forename.getText(), surename.getText(), empDate.getText(), earnings.getText(), baseidInt, workTime.getText(), nip.getText(), account.getText(), DataName.getText(), dataCountryIdInt, dataDetails.getText(), zipCodeInt, city.getText(), tel.getText(), mail.getText());
         }
-
     }
 }
--- src/people/Employee.java
+++ src/people/Employee.java
@@ -12,17 +12,18 @@ public class Employee extends Person{
               String account, String DataName, int dataCountryId, String dataDetails, int zipCode, String city, String tel, String mail)
     {
 
-        String query1 = "INSERT INTO data VALUES " +
-                "(0,"+DataName+","+dataCountryId+","+dataDetails+","+zipCode+","+city+","+tel+","+mail+")";
-        String queryDataId = "SELECT MAX(data_id) FROM data";
+        String query1 = "INSERT INTO data VALUES " + "(0,"+DataName+","+dataCountryId+","+dataDetails+","+zipCode+","+city+","+tel+","+mail+")";
+        //String queryDataId = "SELECT MAX(data_id) FROM data";
         //odpowiedź zapisze do dataId wartość
-        int dataId = 1; //tutaj odpowiedź zamiast jedynki
+        //int dataId = 1; //tutaj odpowiedź zamiast jedynki
         //trzeba mu przypisać baseId
-        String query2 = "INSERT INTO employee VALUES " +
-                "(0,"+login+","+password+","+forename+","+surename+","+dataId+","+empDate+","+earnings+","+baseid+","+workTime+","+false+","+nip+","+account+")";
-        String queryDEmployeeId = "SELECT MAX(employee_id) FROM employee";
-        this.employeeId = 1;//tutaj odpowiedź z serwera.
-
+        //String query2 = "INSERT INTO employee VALUES " + "(0,"+login+","+password+","+forename+","+surename+","+dataId+","+empDate+","+earnings+","+baseid+","+workTime+","+false+","+nip+","+account+")";
+        //String queryDEmployeeId = "SELECT MAX(employee_id) FROM employee";
+        //this.employeeId = 1;//tutaj odpowiedź z serwera.
+        Client.request(query1);
+        //Client.request(queryDataId);
+        //Client.request(query2);
+        //Client.request(queryDEmployeeId);
     }
     @Override
     public void followPackage(int packId) {
