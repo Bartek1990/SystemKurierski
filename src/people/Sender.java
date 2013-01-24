7be582a5ce15fade4fc504dec7fa8e9a90a1aa23
mchrzaszcz <m.s.chrzaszcz@gmail.com>
2013-01-24 18:28:32 +0100

    Tralalal

--- src/people/Sender.java
+++ src/people/Sender.java
@@ -9,6 +9,7 @@ public class Sender extends Person {
         //To change body of implemented methods use File | Settings | File Templates.
     }
     public Pack sendPackage(){
-        Pack pack = new Package();
+          Pack pack = new Pack(10, "Szkło", 5, true, "tralala");  //Przykladowe wartosci
+        return pack;
     }
 }
