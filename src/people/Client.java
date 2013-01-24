dc217a31de9b0ac48d0cc6c95764aff1da3a686f
MateuszGawel <mg.mateuszgawel@gmail.com>
2013-01-24 18:31:35 +0100

    Klasa request

--- src/people/Client.java
+++ src/people/Client.java
@@ -7,6 +7,7 @@ import com.sun.rowset.*;
 
 public class Client
 {
+
     public static final int PORT = 8867;
     public static final String HOST = "localhost";
 
@@ -48,40 +49,55 @@ public class Client
         }
         return "Błąd...";
     }
-    public CachedRowSetImpl communicationMySQL(String arg)
+    public static CachedRowSetImpl request(String kw)
     {
-        //nawiazanie polaczenia z serwerem
-        try {
+        CachedRowSetImpl message = null;
 
+        try
+        {
             Socket polaczenie = new Socket(HOST, PORT);
             System.out.println("Nawiazalem polaczenie: ");
 
             ObjectOutputStream write = new ObjectOutputStream(polaczenie.getOutputStream());
-            //najzwyczajniej puszczam Stringa i reszta zajmuje sie serwer.
-            write.writeObject(arg);
-
             ObjectInputStream read = new ObjectInputStream(polaczenie.getInputStream());
-            CachedRowSetImpl message;
+
+
+            //wysylanie zapytania
+            write.writeObject(kw);
+            //odbior odpowiedzi
             Object test = read.readObject();
 
-            message = (CachedRowSetImpl) test;
-            message.first();
-            System.out.println("Wiadomość: " + message.getString(2));   //po stronie klienta pytamy teraz o to co nas interesuje. Dzialaja tu metody te co na obiekcie ResultSet
+            if(test instanceof String)
+            {
+                System.out.println(test); //jesli to nie bylo zapytanie select to dostaniemy tylko stringa z potwierdzeniem. Trzeba obsluzyc sprawdzenie tego, zeby nie rzutowac Stringa na CachedRowSetImpl.
+            }
+            else
+            {
+                message = (CachedRowSetImpl) test;
+            }
 
-            //jedyne zapytanie jakie będzie zwracac zawartosc bazy to takie zaczynajace sie od "select" (substring) jesli chcesz dodac inne to musisz sobie dodac OR w ifie w QueryManager.java.
             write.close();
             read.close();
             polaczenie.close();
-            return message;
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
+
+        }
+        catch (UnknownHostException e)
+        {
             e.printStackTrace();
         }
-        return null;
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+			/*catch (SQLException e)
+            {
+                e.printStackTrace();
+            }*/
+
+        return message;
     }
 }
