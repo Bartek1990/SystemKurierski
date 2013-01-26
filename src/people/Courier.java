c6e5a5934bcc1d84821e57081a75d77abe4548f6
Bartek Grzebinoga <bartek.grzybek@gmail.com>
2013-01-26 18:21:06 +0100

    Konstruktor dla couriera

--- src/people/Courier.java
+++ src/people/Courier.java
@@ -7,8 +7,11 @@ public class Courier extends Employee {
 
     private int courierID;
 
-    public Courier(Employee courier) throws AlreadyInDbException {
+    public Courier(Employee courier, String companyPhoneNumber) throws AlreadyInDbException {
         super(courier);
+        this.courierID = courier.employeeId;
+        Client.request("INSERT INTO courier VALUES" +
+                "(0, \""+this.employeeId+"\", null, null,\""+companyPhoneNumber+"\")");
 
     }
 	public void setStatus(Package pack) {
